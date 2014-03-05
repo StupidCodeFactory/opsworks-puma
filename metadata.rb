63dd2580b250a615139d0719d994a73b63acb922
Andy Fleener <andrew.fleener@sportngin.com>
2014-03-05 16:04:25 -0600

    Add metadata file

---
+++ metadata.rb
@@ -0,0 +1,13 @@
+name        "opsworks-puma"
+description "Manage the ruby app server puma on AWS OpsWorks"
+maintainer  "Sport Ngin"
+license     "MIT"
+version     "0.0.1"
+
+depends "dependencies"
+depends "puma"
+depends "nginx"
+depends "deploy"
+
+recipe "opsworks-puma::deploy", "Used during the deploy lifecyle event to ensure nginx is configured properly"
+recipe "opsworks-puma", "Setup puma to run for all of a stacks rails applications"
